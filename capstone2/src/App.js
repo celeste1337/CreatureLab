dc0f4897967121d3a2f1fbe58edf412260aa43a9
Celeste Yost <17mcyost@gmail.com>
2022-01-18 15:58:37 -0500

    test component for victoria to goof around on lol

--- capstone2/src/App.js
+++ capstone2/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test.js';
 
 function App() {
+  //note the Test component is in here
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>welcome to our twisted sick demented capstone project..</h1>
       </header>
+
+      
+      <Test></Test>
+
+
+      
     </div>
   );
 }
