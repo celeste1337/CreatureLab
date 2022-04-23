7da6fb0514bc7f94ed6a0da2990f5ec6f3d3f5a3
Celeste Yost <17mcyost@gmail.com>
2022-04-22 22:29:49 -0400

    kjsdnfkjs

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ dbconnect.connectToServer(function(err) {
         process.exit();
     }
     //start server
-    
 });
 
 //serveeeeee
@@ -23,18 +22,9 @@ app.use(cors());
 app.use(bodyparser.raw({limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
 
-
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(require('./routes/endpoints'));
-
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
 
 
 app.listen(port, () => {
