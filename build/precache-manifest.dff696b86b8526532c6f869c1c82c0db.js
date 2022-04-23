25e687cd6422b00f00473c243246d65d0f232159
Celeste Yost <17mcyost@gmail.com>
2022-04-22 22:49:37 -0400

    sdjfns

---
+++ build/precache-manifest.dff696b86b8526532c6f869c1c82c0db.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "74c9769af8d825642fab",
+    "url": "/static/css/main.f7b98fa1.chunk.css"
+  },
+  {
+    "revision": "74c9769af8d825642fab",
+    "url": "/static/js/main.3b4483bb.chunk.js"
+  },
+  {
+    "revision": "a94cb13bd8c3349e145f",
+    "url": "/static/js/runtime~main.a8bb32bb.js"
+  },
+  {
+    "revision": "a1541e89bc6dec7f99bb",
+    "url": "/static/js/2.8e5de2eb.chunk.js"
+  },
+  {
+    "revision": "1ba86b909d0003d76632",
+    "url": "/static/js/3.3b7755ae.chunk.js"
+  },
+  {
+    "revision": "e1e2f9246d643f163f116e0800df040d",
+    "url": "/static/media/Logo.e1e2f924.png"
+  },
+  {
+    "revision": "f8ca35df29b7ad9942cb70113238f5a1",
+    "url": "/static/media/Squigs.f8ca35df.png"
+  },
+  {
+    "revision": "1817ca5c178a15e3ea85b84a5d3cc140",
+    "url": "/static/media/undo-thumb.1817ca5c.PNG"
+  },
+  {
+    "revision": "49bcee8edebd5be70cd14825c9613bd9",
+    "url": "/static/media/purplecreature.49bcee8e.png"
+  },
+  {
+    "revision": "a36b944bb11bfabf8ee5fcc5d7447992",
+    "url": "/static/media/orangecreature.a36b944b.png"
+  },
+  {
+    "revision": "cbb68760ce92cd61e7d1b16dc117290d",
+    "url": "/static/media/Rule-4.cbb68760.gif"
+  },
+  {
+    "revision": "674897aeb0aada04433d5d923d201062",
+    "url": "/static/media/Rule-2.674897ae.gif"
+  },
+  {
+    "revision": "796bc5f311b937d90339b45008c28a64",
+    "url": "/static/media/Rule-5.796bc5f3.gif"
+  },
+  {
+    "revision": "e1d4a7741874e47be18b391b731468af",
+    "url": "/static/media/Rule-3.e1d4a774.gif"
+  },
+  {
+    "revision": "9301ca308af897baffdc8913e4574d95",
+    "url": "/static/media/Rule-1.9301ca30.gif"
+  },
+  {
+    "revision": "5099f4f95da6df41b3af9a6fbe4d1580",
+    "url": "/static/media/BigSquiggle.5099f4f9.svg"
+  },
+  {
+    "revision": "1a77b1d829aaa5abec85652f0a3076e2",
+    "url": "/static/media/MediumSquiggle.1a77b1d8.svg"
+  },
+  {
+    "revision": "cdbec8f089a80dd2e80ab700d9c0eac7",
+    "url": "/static/media/SmallSquiggle.cdbec8f0.svg"
+  },
+  {
+    "revision": "47b5a17db0aa8e08dbcbc92a5dd6d23c",
+    "url": "/static/media/LightBulb.47b5a17d.svg"
+  },
+  {
+    "revision": "f4153480ea1d1fd1e23a1221ca0ef4f3",
+    "url": "/static/media/Trash.f4153480.svg"
+  },
+  {
+    "revision": "971398169bf7294ba3c9b0956d8767a7",
+    "url": "/static/media/Creature.97139816.svg"
+  },
+  {
+    "revision": "7074eff9bea971a6ec9f53883ba68dd0",
+    "url": "/static/media/Lab.7074eff9.svg"
+  },
+  {
+    "revision": "d0b71baae13b052731cb2ba7870647d9",
+    "url": "/static/media/Question.d0b71baa.svg"
+  },
+  {
+    "revision": "003da5cac8c2f5ebcfb4b7f3dcd6792b",
+    "url": "/index.html"
+  }
+];
--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ app.use(express.json({limit: '50mb'}));
 
 app.use(require('./routes/endpoints'));
 
-app.use(express.static(path.join(__dirname, '../client/build/')));
+app.use(express.static(path.join(__dirname, '../build/')));
 app.get('/', (req,res) =>{
     //res.json("hi")
-    res.sendFile(path.join(__dirname+'../client/build/index.html'));
+    res.sendFile(path.join(__dirname+'../build/index.html'));
 });
 
 //err handling
