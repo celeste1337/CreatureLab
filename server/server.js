df8fddcb3d78e415ab9f17ad849bfef145c959e7
Celeste Yost <17mcyost@gmail.com>
2022-04-22 21:50:10 -0400

    djfns

--- server/server.js
+++ server/server.js
@@ -14,28 +14,26 @@ app.use(cors());
 app.use(bodyparser.raw({limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
 
-const apiCalls = require('./routes/endpoints');
-app.use('/api', apiCalls);
-
-app.use(express.static(path.join(__dirname, '../client/build/')));
-app.get('/', (req,res) =>{
-    //res.json("hi")
-    res.sendFile(path.join(__dirname+'../client/build/index.html'));
-});
-
-//err handling
-app.use(function(err, _req, res, next) {
-    console.error(err.stack);
-    res.status(500).send("Something went wrong.");
-});
-
 dbconnect.connectToServer(function(err) {
     if(err) {
         console.error(err);
         process.exit();
     }
-    //start server
-    app.listen(port, () => {
-        console.log('app is listening on port '+port)
-    })
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build')); // serve the static react app
+    app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+      res.sendFile(path.join(__dirname, './client/build/index.html'));
+    });
+    console.log('Serving React App...');
+  };
+
+const apiCalls = require('./routes/endpoints');
+app.use('/api', apiCalls);
+
+
+//start server
+app.listen(port, () => {
+    console.log('app is listening on port '+port)
+})
