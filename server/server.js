f81196407c1a3d07e9fff6b5c9a76428ae3a06ca
Celeste Yost <17mcyost@gmail.com>
2022-04-22 23:01:37 -0400

    dsfjkskj

--- server/server.js
+++ server/server.js
@@ -14,8 +14,9 @@ app.use(cors());
 app.use(bodyparser.raw({limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
 
+const routes = require('./routes/endpoints');
 
-app.use(require('./routes/endpoints'));
+app.use('/api', routes);
 
 app.use(express.static(path.join(__dirname, '../build/')));
 app.get('/', (req,res) =>{
