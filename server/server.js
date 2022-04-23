4ee33397c07c04db08cf125530d9298726198c08
Celeste Yost <17mcyost@gmail.com>
2022-04-22 22:37:52 -0400

    skjndks

--- server/server.js
+++ server/server.js
@@ -9,14 +9,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-dbconnect.connectToServer(function(err) {
-    if(err) {
-        console.error(err);
-        process.exit();
-    }
-    //start server
-});
-
 //serveeeeee
 app.use(cors());
 app.use(bodyparser.raw({limit: '50mb'}));
@@ -24,9 +16,21 @@ app.use(express.json({limit: '50mb'}));
 
 app.use(require('./routes/endpoints'));
 
-app.use(express.static(path.join(__dirname, './../client/build/')));
+app.use(express.static(path.join(__dirname, '../client/build/')));
 
+//err handling
+app.use(function(err, _req, res, next) {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong.");
+});
 
-app.listen(port, () => {
-    console.log('app is listening on port '+port)
-})
+dbconnect.connectToServer(function(err) {
+    if(err) {
+        console.error(err);
+        process.exit();
+    }
+    //start server
+    app.listen(port, () => {
+        console.log('app is listening on port '+port)
+    })
+});
