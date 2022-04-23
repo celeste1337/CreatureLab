b7e643c977cf93768267363e8076c7f33329cf45
Celeste Yost <17mcyost@gmail.com>
2022-04-22 22:33:23 -0400

    jnkjdnf

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyparser.raw({limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
 
-app.use(express.static(path.join(__dirname, './../client/build')));
+app.use(express.static(path.join(__dirname, './../client/build/')));
 
 app.use(require('./routes/endpoints'));
 
