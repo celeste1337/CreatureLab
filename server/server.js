0a45646ee08816055055739ffb250bd98e95cdde
Celeste Yost <17mcyost@gmail.com>
2022-04-22 22:19:36 -0400

    dsfjskjs

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+dbconnect.connectToServer(function(err) {
+    if(err) {
+        console.error(err);
+        process.exit();
+    }
+    //start server
+    
+});
+
 //serveeeeee
 app.use(cors());
 app.use(bodyparser.raw({limit: '50mb'}));
@@ -18,25 +27,16 @@ app.use(express.json({limit: '50mb'}));
 
 app.use(require('./routes/endpoints'));
 
-app.use(express.static(path.join(__dirname, '../build/')));
-app.get('/', (req,res) =>{
-    //res.json("hi")
-    res.sendFile(path.join(__dirname+'/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 
-//err handling
-app.use(function(err, _req, res, next) {
-    console.error(err.stack);
-    res.status(500).send("Something went wrong.");
-});
 
-dbconnect.connectToServer(function(err) {
-    if(err) {
-        console.error(err);
-        process.exit();
-    }
-    //start server
-    app.listen(port, () => {
-        console.log('app is listening on port '+port)
-    })
-});
+app.listen(port, () => {
+    console.log('app is listening on port '+port)
+})
