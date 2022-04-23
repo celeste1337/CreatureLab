93e03204c52837b214a03e943844b505ec93f3ed
Celeste Yost <17mcyost@gmail.com>
2022-04-22 22:35:31 -0400

    kjbf

--- server/server.js
+++ server/server.js
@@ -22,9 +22,9 @@ app.use(cors());
 app.use(bodyparser.raw({limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
 
-app.use(express.static(path.join(__dirname, './../client/build/')));
-
 app.use(require('./routes/endpoints'));
+
+app.use(express.static(path.join(__dirname, './../client/build/')));
 
 
 app.listen(port, () => {
